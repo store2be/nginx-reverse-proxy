c39c7487ffaf455cfbfe2f8afd37fc92396fde38
Sherif <sherif.mowafy@gmail.com>
2019-06-26 15:39:37 +0200

    ✨ Add the option to ignore query params in caching

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,21 @@ echo "Nginx configured with PROXY_URL \"$PROXY_URL\""
 # Set the default value of $CACHE to false
 CACHE="${CACHE:-false}"
 
+# Set the default value of $CACHE_IGNORE_QUERY_PARAMS to false
+CACHE_IGNORE_QUERY_PARAMS="${CACHE_IGNORE_QUERY_PARAMS:-false}"
+
 if test "$CACHE" = "true"; then
   echo "Use a cache"
+
+  if test "$CACHE_IGNORE_QUERY_PARAMS" = "true"; then
+    # Keep the line and consider query params in caching
+    echo "Ignore query parameters in the cache key"
+  else
+    # The default behaviour, delete the line and consider query params in caching
+    echo "[Default behaviour] Don't ignore query parameters in the cache key";
+    sed -i -e 's|proxy_cache_key%||g' /etc/nginx/conf.d/cached.conf
+  fi
+
   # Delete the non cache config
   rm /etc/nginx/conf.d/default.conf
 else
